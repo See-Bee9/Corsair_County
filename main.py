948fe8ae5b397fa515677255427ee18d7045da07
See-Bee9 <colin_bird2000@yahoo.com>
2022-08-10 17:35:52 -0400

    Completed output formatting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from enum import unique
 import scipy.sparse as sparse
-import sys
 
 def normalize(city):
     return '_'.join(city.lower().split())
+
+def denormalize(city):
+    return ' '.join(map(lambda x: x.capitalize(), city.split('_')))
 
 def add_city(city):
     normalized = normalize(city)
@@ -12,11 +14,15 @@ def add_city(city):
 
 def build_array():
     unique_cities = len(dict)
-    matrix = [[9999]*unique_cities]*unique_cities
+    matrix = [[9999 for i in range(unique_cities)] for j in range(unique_cities)]
     i = 0
     for key in dict.keys():
         dict[key] = i
+        name_lookup[i] = key
         i = i + 1
+    for x in range(len(matrix)):
+        cur = matrix[x][x]
+        matrix[x][x] = 0
     return matrix
 
 def add_weight(matrix, source, sink, weight):
@@ -30,15 +36,10 @@ def populate_array(matrix, edges):
         (source, sink, weight) = edge
         add_weight(matrix, source, sink, weight)
 
-
-
-# graph = [[0,1,2],[1,0,5],[2,5,0]]
-# result = sparse.csgraph.floyd_warshall(graph)
-# print(result)
-
 starting = True
 
 dict = {}
+name_lookup = {}
 tuples = []
 
 
@@ -59,12 +60,27 @@ with open('graph.tsv', 'r') as f:
             print("Error parsing line ", line, " : please validate that the format is correct.")
 
 matrix = build_array()
-print(dict)
-print(tuples)
 
 populate_array(matrix, tuples)
 
 result = sparse.csgraph.floyd_warshall(matrix)
-for distance in result:
-    print(distance)
+
+result_grid = [['City']]
+
+for x in range(len(result)):
+    denorm_name = denormalize(name_lookup[x])
+    result_grid[0].append(denorm_name)
+
+for x in range(len(result)):
+    current_house = [denormalize(name_lookup[x])]
+    for y in range(len(result)):
+        current_house.append(int(result[x][y]))
+    result_grid.append(current_house)
+
+
+with open('output.csv',"w") as f:
+    for row in result_grid:
+        csv = ','.join(map(lambda x: str(x),row))
+        f.write(csv)
+        f.write('\n')
         
